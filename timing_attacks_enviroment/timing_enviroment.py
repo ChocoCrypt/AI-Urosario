d88d8119cf682bbadf1ed8f07f6207d91df3e3e1
R <r@localhost.localdomain>
2022-08-15 16:00:04 -0500

    updates from pwnlap on lun 15 ago 2022 16:00:04 -05

--- timing_attacks_enviroment/timing_enviroment.py
+++ timing_attacks_enviroment/timing_enviroment.py
@@ -1,65 +1,19 @@
+"""An implementation of side Side Channel game - Rodrigo Castillo Camargo"""
 import time
 from tqdm import tqdm
 from secret import simple_comp
 from secret import super_secret_password
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-class Side_Channel_Game:
-    """
-    An implementation of a side channel attack by timing as a game
-    enviroment with different algorithms that may solve the problem of password
-    cracking via timing attack.
-    """
+class SideChannel_Game:
     def __init__(self):
-        self.init_time = time.time
-        self.cracked_initial_state = ""
-        # Define alphabet 
-        self.alphabet = [chr(i) for i in range(ord("A") , ord("Z")+1 )] + [" "]
-        self.costo = 0
+        self.password = ""
 
-    def crack_lenght(self):
-        """
-        Function for cracking the lenght of the key. This method test every
-        possible lenght of a key and returns the one that take most time
-        processing.
-        """
-        print("guessing lenght of the key...")
-        base_string = ""
-        means_vector = []
-        for i in tqdm(range(100)):
-            # This attack have sense if we check the mean of the times,
-            # however, this may be incorrect, this method will be improved with
-            # the time using reinforcement learning algorithms
-            proms  = []
-            # Get the mean
-            for i in range(100000):
-                init = time.time()
-                super_secret_password(base_string)
-                fin = time.time()
-                tot = fin-init
-                proms.append(tot)
-            base_string += "A"
-            means_vector.append(np.mean(proms))
-        lenght = np.argmax(means_vector)
-        print(f"the lenght of the key is {lenght}")
-        return(lenght)
-
-    def test_objetivo(self):
-        """Function for testing if the password is already correct"""
-        if(super_secret_password(self.cracked_initial_state)):
-            print("Its Done")
-            return True
-        return False
+    def pintar_estado(self , estado):
+        print(self.password)
 
 
-    def __str__(self):
-        """This method is equivalent to the method called pintar_estado"""
-        print(self.cracked_state)
-
-
-
-
-
-test = Side_Channel_Game()
-test.crack_lenght()
+print(np.matrix([[0] *8]*8 ) )
