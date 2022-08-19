264aa94176e0d57d26c88080fee4cf9d5a4702d2
R <r@localhost.localdomain>
2022-08-19 17:00:06 -0500

    updates from pwnlap on vie 19 ago 2022 17:00:06 -05

--- timing_attacks_enviroment/secret.py
+++ timing_attacks_enviroment/secret.py
@@ -19,7 +19,7 @@ def simple_comp(str1, str2):
 
 def super_secret_password(user_input):
     """Secret password comparisson against the given string."""
-    secret = "GOOD"
+    secret = "BIEN"
     if(simple_comp(user_input, secret)):
         return True
     return False
